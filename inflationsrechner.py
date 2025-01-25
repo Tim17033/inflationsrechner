d9811bdc806787a662c39c862bf3890bf2ea159c
Tim17033 <wachtim9@gmail.com>
2025-01-25 11:39:47 +0100

    Update inflationsrechner.py

--- inflationsrechner.py
+++ inflationsrechner.py
@@ -41,7 +41,7 @@ if st.button("📊 Berechnung starten"):
         with col1:
             st.metric(label="💼 Startbetrag", value=f"{startbetrag:,.2f} €")
         with col2:
-            st.metric(label=f"📉 Betrag nach {zeitraum} Jahren (ohne Zinsen)", value=f"{endbetrag:,.2f} €", delta=f"-{inflationsverlust:,.2f} €", delta_color="inverse")
+            st.metric(label=f"📉 Betrag nach {zeitraum} Jahren (ohne Zinsen)", value=f"{endbetrag:,.2f} €", delta=f"-{inflationsverlust:,.2f} €", delta_color="normal")
         with col3:
             st.metric(label=f"📈 Betrag mit {zinsrate:.2f}% Zinsen", value=f"{endbetrag_mit_zins:,.2f} €", delta=f"-{kaufkraftverlust_mit_zins:,.2f} €", delta_color="inverse")
 
@@ -76,6 +76,7 @@ if st.button("📊 Berechnung starten"):
         - Tatsächliche Werte können aufgrund wirtschaftlicher Schwankungen abweichen.
         - Die grafische Darstellung hilft Ihnen, den Einfluss der Inflation auf Ihre Ersparnisse besser zu verstehen.
         """)
+
 
 
 
